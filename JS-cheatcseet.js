c236b54e1a221430bf9ed4e919f23fdd8058cddc
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2020-04-05 20:14:45 +0530

    Update JS-cheatcseet.js

--- JS-cheatcseet.js
+++ JS-cheatcseet.js
@@ -1,1 +1,16 @@
 JAVASCRIPT CHEAT SHEET !!
+
+
+//// we can connect the javascript file using src attribute in script tag 
+<script src="*NAME OF THE FILE *"></script>
+
+
+/////to add javascript by id use script tag 
+<script></script>   //used in body tag 
+
+
+////// to get html element by id just use 
+document.getElementById("*ID GOES HERE*")
+
+
+//////
