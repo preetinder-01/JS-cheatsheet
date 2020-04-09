1d4b37c2e627cfe0166976c7cbbd38fb554704c6
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2020-04-09 13:09:22 +0530

    Update JS-cheatcseet.js

--- JS-cheatcseet.js
+++ JS-cheatcseet.js
@@ -13,4 +13,29 @@ JAVASCRIPT CHEAT SHEET !!
 document.getElementById("*ID GOES HERE*")
 
 
-//////
+//////to create a variable 
+use var NAME;                   use "" or '' to store a character or string to a variable
+                                no need to use quotes for true or false as it is just a datatype
+/////can also use 
+var name1,name2,name3;          variable must begin with the uppercase letter lowercase letter ,underscore or dollar sign
+        or                      use camelCase
+var name1=VALUE,
+    name2=VALUE,
+    age;        
+
+///// we can use math library to perform some functions
+Math.max(a,b);
+Math.sqrt(a,b);
+Math.round(a,ab);
+
+/// OPERAtORS    
+==  EQUALS TO 
+<
+>
+
+
+// COMPARISON OF STRINGS
+"STRING1" > "STRING2";
+it will just compare the strings alphabet by alphabet and check which comes first
+ALL CAPITAL LETTERS COME BEFORE THE LOWER CASE LETTERS
+
